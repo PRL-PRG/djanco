265273ab8d0918ea7b282a95ba7b8b35a858be65
Konrad Siek <konrad.siek@gmail.com>
2020-07-10 17:53:54 +0200

    Commits.

--- src/ast.rs
+++ src/ast.rs
@@ -1,40 +1,38 @@
-/**
- * A query consists of either:
- *  - a single feature: commits(...)
- *  - a tree of features joined by connectives: commit(...) && (files(...) && ...)
- * Since the tree is recursive, the query is boxed.
- */
 #[derive(PartialEq,Debug,Clone)]
-pub enum Query {
-    Simple(Feature),
-    Compound(Feature, Connective, Box<Query>),
+pub struct Query {
+    //assignments: Vector<Assignment>
+    expression: Expression,
 }
 
-/**
- * Connectives between features.
- */
+#[derive(PartialEq,Debug,Clone)]
+pub struct Expression {
+    head: Feature,
+    tail: Vec<(Connective, Feature)>,
+}
+
 #[derive(PartialEq,Debug,Clone)]
 pub enum Connective {
     Conjunction,
+    //Disjunction
 }
 
 #[derive(PartialEq,Debug,Clone)]
-pub struct Feature {
-    name: String,                               // replace with a tag if it matters for performance
-    filters: Vector<Filter>,
-    properties: Vector<Calculation>
+pub enum Feature {
+    Commits   { parameters: Vec<Parameter>, property: Option<Property> },
+    Additions { parameters: Vec<Parameter>, property: Option<Property> },
+    Deletions { parameters: Vec<Parameter>, property: Option<Property> },
+    Changes   { parameters: Vec<Parameter>, property: Option<Property> },
 }
 
 #[derive(PartialEq,Debug,Clone)]
-pub struct Filter {
-    name: String,
-    operator: Operator,
-    value: Value,
+pub enum Parameter {
+    Path { operator: StringOperator, pattern: String }
 }
 
 #[derive(PartialEq,Debug,Clone)]
 pub enum Property {
     ElapsedTime,
+    //Percentile(u32),
 }
 
 #[derive(PartialEq,Debug,Clone)]
@@ -53,14 +51,110 @@ pub enum RelationalOperator {
     GreaterEqual,
 }
 
-#[derive(PartialEq,Debug,Clone)]
-pub enum Value {
-    Glob(String), // eg. "test/*"
+impl Query {
+    pub fn simple (expression: Expression) -> Query {
+        Query {expression}
+    }
 }
 
-pub enum FeatureTag {
-    Commits,
-    Additions,
-    Deletions,
-    Changes,
+impl Expression {
+    pub fn head (head: Feature) -> Expression {
+        Expression { head, tail: Vec::new() }
+    }
+    pub fn new (head: Feature, tail: Vec<(Connective, Feature)>) -> Expression {
+        Expression { head, tail }
+    }
 }
+
+impl Feature {
+    pub fn commits_simple() -> Feature {
+        Feature::Commits { parameters: Vec::new(), property: None }
+    }
+    pub fn commits_with_parameter(parameter: Parameter) -> Feature {
+        Feature::Commits { parameters: vec![parameter], property: None }
+    }
+    pub fn commits_with_parameters(parameters: Vec<Parameter>) -> Feature {
+        Feature::Commits { parameters, property: None }
+    }
+    pub fn commits_with_property(property: Property) -> Feature {
+        Feature::Commits { parameters: Vec::new(), property: Some(property) }
+    }
+    pub fn commits(parameters: Vec<Parameter>, property: Property) -> Feature {
+        Feature::Commits { parameters, property: Some(property) }
+    }
+
+    pub fn additions_simple() -> Feature {
+        Feature::Additions { parameters: Vec::new(), property: None }
+    }
+    pub fn additions_with_parameter(parameter: Parameter) -> Feature {
+        Feature::Additions { parameters: vec![parameter], property: None }
+    }
+    pub fn additions_with_parameters(parameters: Vec<Parameter>) -> Feature {
+        Feature::Additions { parameters, property: None }
+    }
+    pub fn additions_with_property(property: Property) -> Feature {
+        Feature::Additions { parameters: Vec::new(), property: Some(property) }
+    }
+    pub fn additions(parameters: Vec<Parameter>, property: Property) -> Feature {
+        Feature::Additions { parameters, property: Some(property) }
+    }
+
+    pub fn deletions_simple() -> Feature {
+        Feature::Deletions { parameters: Vec::new(), property: None }
+    }
+    pub fn deletions_with_parameter(parameter: Parameter) -> Feature {
+        Feature::Deletions { parameters: vec![parameter], property: None }
+    }
+    pub fn deletions_with_parameters(parameters: Vec<Parameter>) -> Feature {
+        Feature::Deletions { parameters, property: None }
+    }
+    pub fn deletions_with_property(property: Property) -> Feature {
+        Feature::Deletions { parameters: Vec::new(), property: Some(property) }
+    }
+    pub fn deletions(parameters: Vec<Parameter>, property: Property) -> Feature {
+        Feature::Deletions { parameters, property: Some(property) }
+    }
+
+    pub fn changes_simple() -> Feature {
+        Feature::Changes { parameters: Vec::new(), property: None }
+    }
+    pub fn changes_with_parameter(parameter: Parameter) -> Feature {
+        Feature::Changes { parameters: vec![parameter], property: None }
+    }
+    pub fn changes_with_parameters(parameters: Vec<Parameter>) -> Feature {
+        Feature::Changes { parameters, property: None }
+    }
+    pub fn changes_with_property(property: Property) -> Feature {
+        Feature::Changes { parameters: Vec::new(), property: Some(property) }
+    }
+    pub fn changes(parameters: Vec<Parameter>, property: Property) -> Feature {
+        Feature::Changes { parameters, property: Some(property) }
+    }
+}
+
+impl Parameter {
+    pub fn path_equal(pattern: String) -> Parameter {
+        Parameter::Path { operator: StringOperator::Equal, pattern }
+    }
+    pub fn path_different(pattern: String) -> Parameter {
+        Parameter::Path { operator: StringOperator::Different, pattern }
+    }
+    pub fn path_equal_str(pattern: &str) -> Parameter {
+        Parameter::Path { operator: StringOperator::Equal, pattern: pattern.to_string() }
+    }
+    pub fn path_different_str(pattern: &str) -> Parameter {
+        Parameter::Path { operator: StringOperator::Different, pattern: pattern.to_string() }
+    }
+}
+
+#[macro_export]
+macro_rules! compose_parameters {
+    ( $head: expr, $tail: expr ) => {{
+        let mut parameters = Vec::new();
+        parameters.push($head);
+        let clean_tail: Vec<Parameter> = $tail.iter().map(|e| e.1.clone()).collect();
+        parameters.extend(clean_tail);
+        parameters
+    }}
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,81 @@
+mod ast;
+
 #[macro_use]
 extern crate lalrpop_util;
 
-lalrpop_mod!(pub fml); // synthesized by LALRPOP
+lalrpop_mod!(pub grammar); // synthesized by LALRPOP
+
+pub fn parse(input: &str) -> Result<ast::Query, String> {
+    match grammar::QUERYParser::new().parse(input) {
+        Err(e) => Err(format!("{:?}", e)),
+        Ok(ast) => Ok(ast),
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", parse("commits"));
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parse;
+    use crate::ast::{Query, Expression, Feature, Parameter, Property};
+
+    fn wrap(feature: Feature) -> Query {
+        Query::simple(Expression::head(feature))
+    }
+
+    fn parse_ok(input: &str, expected: Query) {
+        assert_eq!(parse(input), Ok(expected));
+    }
+
+    #[test] fn test_commits() {
+        parse_ok("commits",
+                 wrap(Feature::commits_simple()))
+    }
+
+    #[test] fn test_commits_with_empty_parens() {
+        parse_ok("commits()",
+                 wrap(Feature::commits_simple()))
+    }
+
+    #[test] fn test_commits_with_equal_path_filter() {
+        let parameter = Parameter::path_equal_str("test/*");
+        parse_ok(r#"commits(path=="test/*")"#,
+                 wrap(Feature::commits_with_parameter(parameter)))
+    }
+
+    #[test] fn test_commits_with_different_path_filter() {
+        let parameter = Parameter::path_different_str("test/*");
+        parse_ok(r#"commits(path!="test/*")"#,
+                 wrap(Feature::commits_with_parameter(parameter)))
+    }
+
+    #[test] fn test_commits_with_extra_comma() {
+        let parameter = Parameter::path_different_str("test/*");
+        parse_ok(r#"commits(path!="test/*",)"#,
+                 wrap(Feature::commits_with_parameter(parameter)))
+    }
+
+    #[test] fn test_commits_with_elapsed_time() {
+        parse_ok("commits.elapsedTime",
+                 wrap(Feature::commits_with_property(Property::ElapsedTime)))
+    }
+
+    #[test] fn test_commits_with_elapsed_time_and_empty_parens_() {
+        parse_ok("commits().elapsedTime()",
+                 wrap(Feature::commits_with_property(Property::ElapsedTime)))
+    }
+
+    #[test] fn test_commits_with_equal_path_filter_and_elapsed_time() {
+        let parameter = Parameter::path_equal_str("test/*");
+        parse_ok(r#"commits(path=="test/*").elapsedTime"#,
+                 wrap(Feature::commits(vec![parameter], Property::ElapsedTime)))
+    }
+
+    #[test] fn test_commits_with_different_path_filter_and_elapsed_time() {
+        let parameter = Parameter::path_different_str("test/*");
+        parse_ok(r#"commits(path!="test/*").elapsedTime"#,
+                 wrap(Feature::commits(vec![parameter], Property::ElapsedTime)))
+    }
 }

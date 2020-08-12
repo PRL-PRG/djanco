253b19c17c0d1300de64d85a0efcafdc377d4625
Konrad Siek <konrad.siek@gmail.com>
2020-08-12 15:58:56 +0200

    Options from commandline.
    How I learned to stop worrying and love the crates.

--- select/src/bin/select.rs
+++ select/src/bin/select.rs
@@ -1,4 +1,4 @@
-use std::time::Instant;
+use structopt::StructOpt;
 
 use dcd::{DCD, Database};
 
@@ -7,17 +7,34 @@ use select::meta::ProjectMeta;
 
 macro_rules! with_elapsed_seconds {
     ($thing:expr) => {{
-        let start = Instant::now();
+        let start = std::time::Instant::now();
         let result = { $thing };
         (result, start.elapsed().as_secs())
     }}
 }
 
-fn main() {
-    let (database, loading_time) =
-        with_elapsed_seconds!(DCD::new("/dejavuii/dejacode/dataset-tiny".to_string()));
+#[derive(StructOpt)]
+struct Configuration {
+    #[structopt(parse(from_os_str), short = "o", long = "output")]
+    output_path: std::path::PathBuf,
 
-    let (projects, query_execution_time) =
+    #[structopt(parse(from_os_str), short = "d", long = "dataset")]
+    dataset_path: std::path::PathBuf,
+}
+
+impl Configuration {
+    fn dataset_path_as_string(&self) -> String {
+        self.dataset_path.as_os_str().to_str().unwrap().to_string()
+    }
+}
+
+fn main() {
+    let configuration = Configuration::from_args();
+
+    let (database, loading_time) =
+        with_elapsed_seconds!(DCD::new(configuration.dataset_path_as_string()));
+
+    let (projects, query_time) =
         with_elapsed_seconds!(group_by_language_order_by_stars_top_n(&database, 10));
 
     for project_id in projects {
@@ -25,5 +42,5 @@ fn main() {
         println!("{:?} {:?} {:?}", project_id, project.get_language(), project.get_stars())
     }
 
-    eprintln!("elapsed time: loading = {}s, query = {}s", loading_time, query_execution_time);
+    eprintln!("(elapsed time: loading={}s, query={}s)", loading_time, query_time);
 }

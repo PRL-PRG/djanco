9d3ce47ba965287ce18ee276e12169dd2747fb2c
Konrad Siek <konrad.siek@gmail.com>
2020-09-08 10:20:24 +0200

    Dead end

--- select/src/bin/sandbox.rs
+++ select/src/bin/sandbox.rs
@@ -1,3 +1,15 @@
+use dcd::Project;
+
 fn main() {
-    println!("{:?}", Some("a".to_string()) == Some("a".to_string()));
+    fn my_query(projects: impl Iterator<Item=Project>) -> impl Iterator<Item=Project> {
+        Box::new(projects)
+    }
+
+    fn bla<F,I>(f: F) -> () where F: FnMut(I) -> I, I: Iterator<Item=Project> {
+        let projects: Vec::<Project> = vec![];
+        let selection: Vec::<Project> = f(projects.iter()).collect();
+
+    }
+
+    bla(my_query)
 }

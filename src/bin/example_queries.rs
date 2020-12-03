a1d19b4f1185fc4223dcbe815499c2767b31a1a5
Konrad Siek <konrad.siek@gmail.com>
2020-12-03 17:28:08 +0100

    One more query possible.

--- src/bin/example_queries.rs
+++ src/bin/example_queries.rs
@@ -52,6 +52,25 @@ fn experienced_authors_ratio<'a>(_config: &Configuration, _log: &Log, database: 
         .ungroup()
 }
 
+
+fn mean_commit_message_sizes<'a>(_config: &Configuration, _log: &Log, database: &'a Database) -> impl Iterator<Item=ItemWithData<'a, Project>> {
+    database
+        .projects()
+        .group_by_attrib(project::Language)
+        .sort_by_attrib(stats::Mean(get::FromEach(project::Commits, commit::MessageLength)))
+        .sample(sample::Distinct(sample::Top(50), sample::Ratio(project::Commits, 0.9)))
+        .ungroup()
+}
+
+fn median_commit_message_sizes<'a>(_config: &Configuration, _log: &Log, database: &'a Database) -> impl Iterator<Item=ItemWithData<'a, Project>> {
+    database
+        .projects()
+        .group_by_attrib(project::Language)
+        .sort_by_attrib(stats::Median(get::FromEach(project::Commits, commit::MessageLength)))
+        .sample(sample::Distinct(sample::Top(50), sample::Ratio(project::Commits, 0.9)))
+        .ungroup()
+}
+
 // works with downloader from commit 5e4e9d5deb0fe8f9c8bb3bae0ca6947633701346 
 // `cargo run --bin example --release -- -o ~/output -d /mnt/data/dataset -c /mnt/data/cache --data-dump=~/output/dump`
 fn main() {
@@ -59,12 +78,15 @@ fn main() {
     let config = Configuration::from_args();
     let log = Log::new(Verbosity::Debug);
 
+    macro_rules! path { ($name:expr) => { config.output_csv_path($name) } }
+
     let store = DatastoreView::new(config.dataset_path(), now);
     let database = Database::from_store(store, config.cache_path(), log.clone());
 
-    stars(&config, &log, &database).into_csv(config.output_csv_path("stars")).unwrap();
-    mean_changed_paths(&config, &log, &database).into_csv(config.output_csv_path("mean_changed_paths")).unwrap();
-    median_changed_paths(&config, &log, &database).into_csv(config.output_csv_path("median_changed_paths")).unwrap();
-    experienced_authors_ratio(&config, &log, &database).into_csv(config.output_csv_path("experienced_authors_ratio")).unwrap();
-
+    stars(&config, &log, &database).into_csv(path!("stars")).unwrap();
+    mean_changed_paths(&config, &log, &database).into_csv(path!("mean_changed_paths")).unwrap();
+    median_changed_paths(&config, &log, &database).into_csv(path!("median_changed_paths")).unwrap();
+    experienced_authors_ratio(&config, &log, &database).into_csv(path!("experienced_authors_ratio")).unwrap();
+    mean_commit_message_sizes(&config, &log, &database).into_csv(path!("mean_commit_message_sizes")).unwrap();
+    median_commit_message_sizes(&config, &log, &database).into_csv(path!("median_commit_message_sizes")).unwrap();
 }

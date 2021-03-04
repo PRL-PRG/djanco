d1849532200cc228734451275e2482bc45dd52ff
Konrad Siek <konrad.siek@gmail.com>
2021-03-04 11:51:09 +0100

    Facade for parasite mk3. That works!

--- src/source.rs
+++ src/source.rs
@@ -5,6 +5,30 @@ use anyhow::*;
 use crate::objects::SnapshotId;
 use std::collections::HashMap;
 use parasite::Metadata;
+use serde_json::Value as JSON;
+use std::str::FromStr;
+
+macro_rules! convert {
+    ($type:ident from $id:expr) => {
+        objects::$type::from(Into::<u64>::into($id))
+    };
+    (($type0:ident, $type1:ident) from $pair:expr) => {
+        (convert!($type0 from $pair.0), convert!($type1 from $pair.1))
+    };
+    (Vec<$type:ident> from $collection:expr) => {
+        $collection.into_iter().map(|id| convert!($type from id)).collect()
+    };
+    (HashMap<$key_type:ident, $value_type:ident> from $collection:expr) => {
+        $collection.into_iter().map(|entry| convert!(($key_type, $value_type) from entry)).collect()
+    };
+}
+
+macro_rules! parse_github_json {
+    ($entity_name:expr, $id:expr, $string:expr) => {
+        JSON::from_str($string.as_str())
+           .with_context(|| format!("Cannot read GitHub metadata for {} {}", $entity_name, $id));
+    }
+}
 
 pub struct Source {
     store: parasite::DatastoreView,
@@ -26,67 +50,113 @@ impl Source {
         self.store.project_urls()
             .into_iter()
             .map(|(id, url)| {
-                (objects::ProjectId::from(Into::<u64>::into(id)), url.clone_url())
+                (convert!(ProjectId from id), url.clone_url())
             })
     }
 
     pub fn project_heads(&self) -> impl Iterator<Item=(objects::ProjectId, HashMap<String, (objects::CommitId, String)>)> {
         self.store.project_heads()
             .into_iter()
-            .map(|(id, heads)| {
+            .map(|(project_id, heads)| {
                 let heads = heads.into_iter()
                     .map(|(name, (commit_id, hash))| {
-                        let commit_id =
-                            objects::CommitId::from(Into::<u64>::into(commit_id));
-                        let hash = hash.to_string();
-                        (name, (commit_id, hash))
+                        (name, (convert!(CommitId from commit_id), hash.to_string()))
                     })
                     .collect::<HashMap<String, (objects::CommitId, String)>>();
-                (objects::ProjectId::from(Into::<u64>::into(id)), heads)
+                (convert!(ProjectId from project_id), heads)
             })
     }
 
-    pub fn project_github_metadata(&self) -> impl Iterator<Item=(objects::ProjectId, String)> {     // TODO JSON
+    pub fn project_github_metadata(&self) -> impl Iterator<Item=(objects::ProjectId, Result<JSON>)> {
         self.store.project_metadata()
             .into_iter()
             .filter(|(_, metadata)| metadata.key == Metadata::GITHUB_METADATA)
             .map(|(id, metadata)| {
-                (objects::ProjectId::from(Into::<u64>::into(id)), metadata.value)
+                (convert!(ProjectId from id), parse_github_json!("Project", id, metadata.value))
             })
     }
 
     pub fn commit_hashes(&self) -> impl Iterator<Item=(objects::CommitId, String)> {
         self.store.commits(self.substore)
             .into_iter()
-            .map(|(id, sha)| {
-                (objects::CommitId::from(Into::<u64>::into(id)), sha.to_string())
-            })
+            .map(|(id, sha)| (convert!(CommitId from id), sha.to_string()))
     }
 
-    pub fn commits_info(&self) -> impl Iterator<Item=(objects::CommitId, ())> {
+    pub fn commit_info(&self) -> impl Iterator<Item=(objects::CommitId, CommitBasics)> {
         self.store.commits_info(self.substore)
             .into_iter()
-            .map(|(id, info)| {
-                (objects::CommitId::from(Into::<u64>::into(id)), ())                  // TODO
+            .map(|(commit_id, info)| {
+                let commit_basics = CommitBasics {
+                    committer: convert!(UserId from info.committer),
+                    committer_time: info.committer_time,
+                    author: convert!(UserId from info.author),
+                    author_time: info.author_time,
+                    parents: convert!(Vec<CommitId> from info.parents),
+                    changes: convert!(HashMap<PathId, SnapshotId> from info.changes),
+                    message: info.message,
+                };
+                (convert!(CommitId from commit_id), commit_basics)
             })
     }
 
-    pub fn commit_github_metadata(&self) -> impl Iterator<Item=(objects::CommitId, String)> {       // TODO JSON
+    pub fn commit_github_metadata(&self) -> impl Iterator<Item=(objects::CommitId, Result<JSON>)> {
         self.store.commits_metadata(self.substore)
             .into_iter()
             .filter(|(_, metadata)| metadata.key == Metadata::GITHUB_METADATA)
             .map(|(id, metadata)| {
-                (objects::CommitId::from(Into::<u64>::into(id)), metadata.value)
+                (convert!(CommitId from id), parse_github_json!("Commit", id, metadata.value))
             })
     }
 
-    // FIXME
+    // TODO hashes?
+
     pub fn snapshot_bytes(&self) -> impl Iterator<Item=(objects::SnapshotId, Vec<u8>)> {
-        unimplemented!();
         self.store.contents(self.substore)
             .into_iter()
             .map(|(id, contents)| {
-                (objects::SnapshotId::from(0u64), contents)
+                (convert!(SnapshotId from id), contents)
             })
     }
+
+    pub fn snapshot_metadata(&self) -> impl Iterator<Item=(objects::SnapshotId, Result<JSON>)> {
+        self.store.contents_metadata(self.substore)
+            .into_iter()
+            .map(|(id, metadata)| {
+                (convert!(SnapshotId from id), parse_github_json!("Snapshot", id, metadata.value))
+            })
+    }
+
+    pub fn path_hashes(&self) -> impl Iterator<Item=(objects::PathId, String)> {
+        self.store.paths(self.substore).into_iter().map(|(id, hash)| {
+            (convert!(PathId from id), hash.to_string())
+        })
+    }
+
+    pub fn paths(&self) -> impl Iterator<Item=(objects::PathId, String)> {
+        self.store.paths_strings(self.substore).into_iter().map(|(id, path)| {
+            (convert!(PathId from id), path)
+        })
+    }
+
+    pub fn user_emails(&self) -> impl Iterator<Item=(objects::UserId, String)> {
+        self.store.users(self.substore).into_iter().map(|(id, email)| {
+            (convert!(UserId from id), email)
+        })
+    }
+
+    pub fn user_metadata(&self) -> impl Iterator<Item=(objects::UserId, Result<JSON>)> {
+        self.store.users_metadata(self.substore).into_iter().map(|(id, metadata)| {
+            (convert!(UserId from id), parse_github_json!("User", id, metadata.value))
+        })
+    }
+}
+
+pub struct CommitBasics {
+    pub committer : objects::UserId,
+    pub committer_time : i64,
+    pub author : objects::UserId,
+    pub author_time : i64,
+    pub parents : Vec<objects::CommitId>,
+    pub changes : HashMap<objects::PathId, objects::SnapshotId>,
+    pub message : String,
 }

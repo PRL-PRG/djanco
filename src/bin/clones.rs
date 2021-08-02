882badcaf5fdcbbdc6ada8dafe362378e8513f21
peta <peta.maj82@gmail.com>
2021-08-02 13:08:21 +0200

    clones do 4 smaller tables so that hopefully we won't OOME

--- src/bin/clones.rs
+++ src/bin/clones.rs
@@ -16,7 +16,83 @@ fn main() {
         Djanco::from_config(&config, timestamp!(December 2020), stores!(Generic), log.clone())
             .expect("Error initializing datastore.");
 
-    projects_all(&config, &log, &database).into_extended_csv_in_dir(&config.output_path, "projects_all").unwrap();
+    //projects_all(&config, &log, &database).into_extended_csv_in_dir(&config.output_path, "projects_all").unwrap();
+    // for lack of better names
+    // for lack of better tools
+    // for lack of better languages
+    // variadic stuff rulez!!!!!!!!! 
+    // but this helps the memory down
+    // constructive comment: We split the table into multiple tables as the macros & traits required for the thing to be glued together properly are only defined up to certain length
+    projects_all(&config, &log, &database).map(|project| {
+        return (
+            project.id(),
+            project.url(),
+            project.is_archived(), 
+            project.star_count(),
+            project.subscriber_count(),
+            project.size(),
+            project.commit_count(),
+            project.license(),
+            project.language(),
+            project.major_language(),
+        );        
+    }).into_csv_with_headers_in_dir(
+        vec!["id","url", "archived","stars","subscribers", "size","commits","license","language","major_language"],
+        &config.output_path,
+        "projects_codedj_2_a"
+    ).unwrap();
+    projects_all(&config, &log, &database).map(|project| {
+        return (
+            project.id(),
+            project.created(),
+            project.lifetime(),
+            project.user_count(),
+            project.author_count(),
+            project.committer_count(),
+            project.max_commit_delta(),
+            project.avg_commit_delta(),
+            project.project_max_experience(),
+            project.project_experience(),
+        );        
+    }).into_csv_with_headers_in_dir(
+        vec!["id","created","age","users","authors","committers","max_c_delta", "avg_c_delta", "max_exp", "exp"],
+        &config.output_path,
+        "projects_codedj_2_b"
+    ).unwrap();
+    projects_all(&config, &log, &database).map(|project| {
+        return (
+            project.id(),
+            project.duplicated_code(),
+            project.authors_contributing_commits_count(95),
+            project.authors_contributing_commits_count(80),
+            project.authors_contributing_commits_count(50),
+            project.authors_contributing_changes_count(95),
+            project.authors_contributing_changes_count(80),
+            project.authors_contributing_changes_count(50),
+            project.unique_files(),
+            project.original_files(),
+        );        
+    }).into_csv_with_headers_in_dir(
+        vec!["id", "dup","a_commits_95","a_commits_80","a_commits_50", "a_changes_95", "a_changes_80", "a_changes_50", "unique", "original"],
+        &config.output_path,
+        "projects_codedj_2_c"
+    ).unwrap();
+    projects_all(&config, &log, &database).map(|project| {
+        return (
+            project.id(),
+            project.path_count(),
+            project.snapshot_count(),
+            project.major_language_ratio(),
+            project.all_forks(),
+            project.project_locs(),
+            project.impact(),
+
+        );        
+    }).into_csv_with_headers_in_dir(
+        vec!["id", "paths", "snapshots","major_language_ratio", "all_forks", "locs", "impact"],
+        &config.output_path,
+        "projects_codedj_2_d"
+    ).unwrap();
     /*
     //snapshots_by_num_projects(&config, &log, &database).into_csv(path!("snapshots_by_projects")).unwrap();
     projects_by_unique_files(&config, &log, &database).into_csv(path!("projects_by_unique_files")).unwrap();
